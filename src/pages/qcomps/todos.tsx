40c471a869e3f74eb20673d14185592ca39c3303
varunmohan05 <varunmohan05@gmail.com>
2024-09-29 16:21:06 -0400

    Fixed bios and todos

--- src/pages/qcomps/todos.tsx
+++ src/pages/qcomps/todos.tsx
@@ -14,8 +14,8 @@ export default function TodoList() {
     <div style={person.theme}>
       <h1>Person Name's Todos</h1>
       <img className="avatar"
-      src="baseURL + imageID + imageSize + .jpg"
-      alt="Person's Name"
+      src={`${baseUrl}${person.imageId}${person.imageSize}.jpg`}
+      alt={person.name}
       />
     </div>
   );
