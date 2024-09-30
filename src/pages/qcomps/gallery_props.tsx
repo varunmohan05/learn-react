a40914f7f91b90fb30dac943e5e839cdece74c40
varunmohan05 <varunmohan05@gmail.com>
2024-09-30 15:51:17 -0400

    Fixed gallery_props.tsx

--- src/pages/qcomps/gallery_props.tsx
+++ src/pages/qcomps/gallery_props.tsx
@@ -1,55 +1,78 @@
+type person = {
+  name: string;
+  profession: string;
+  awardsDescription: string;
+  discoveryDescription: string;
+};
+
+type ProfileProps = { person: person; imageSrc: string; size: number };
+
+const maria: person = {
+  name: "Maria Skłodowska-Curie",
+  profession: "physicist and chemist",
+  awardsDescription:
+    "Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal",
+  discoveryDescription: "polonium (element)",
+};
+
+const katsuko: person = {
+  name: "Katsuko Saruhashi",
+  profession: "geochemist",
+  awardsDescription: "Miyake Prize for geochemistry, Tanaka Prize",
+  discoveryDescription: "a method for measuring carbon dioxide in seawater",
+};
+
+function ListItem(props: { label: string; content: string }) {
+  return (
+    <li>
+      <b>{props.label}</b>
+      {props.content}
+    </li>
+  );
+}
+
+function Profile(props: ProfileProps) {
+  return (
+    <section className="profile">
+      <h2>{props.person.name}</h2>
+      <img
+        className="avatar"
+        src={props.imageSrc}
+        alt={props.person.name}
+        width={props.size}
+        height={props.size}
+      />
+      <ul>
+        <ListItem label="Profession: " content={props.person.profession} />
+        <ListItem
+          label={
+            "Awards: " + props.person.awardsDescription.split(",").length + " "
+          }
+          content={props.person.awardsDescription}
+        />
+        <ListItem
+          label="Discovered: "
+          content={props.person.discoveryDescription}
+        />
+      </ul>
+    </section>
+  );
+}
+
 export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+      <Profile
+        person={maria}
+        imageSrc="https://i.imgur.com/szV5sdGs.jpg"
+        size={70}
+      />
+      <Profile
+        person={katsuko}
+        imageSrc="https://i.imgur.com/YfeOqp2s.jpg"
+        size={70}
+      />
     </div>
   );
 }
