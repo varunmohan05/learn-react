dc578ce8b3f88a8305e8575f8f5ee5b5c42e188f
varunmohan05 <varunmohan05@gmail.com>
2024-10-06 22:58:57 -0400

    Fixed artistsRemoveArr.tsx

--- src/pages/qcomps/artistsRemoveArr.tsx
+++ src/pages/qcomps/artistsRemoveArr.tsx
@@ -1,26 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 let initialArtists = [
-  { id: 0, name: 'Marta Colvin Andrade' },
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
 ];
 
 export default function List() {
-  const [artists, setArtists] = useState(
-    initialArtists
-  );
+  const [artists, setArtists] = useState(initialArtists);
 
   return (
     <>
       <h1>Inspiring sculptors:</h1>
       <ul>
-        {artists.map(artist => (
+        {artists.map((artist) => (
           <li key={artist.id}>
-            {artist.name}{' '}
-            <button onClick={() => {
-              artists.splice(artist.id, 1)
-            }}>
+            {artist.name}{" "}
+            <button
+              onClick={() => {
+                const nextArtists = artists.filter((a) => a.id !== artist.id);
+                setArtists(nextArtists);
+              }}
+            >
               Delete
             </button>
           </li>
