326fc17c7925dd7bb98f0c4752ae660d2edd744d
varunmohan05 <varunmohan05@gmail.com>
2024-10-06 22:51:45 -0400

    Fixed updObjectsForm.tsx

--- src/pages/qcomps/updObjectsForm.tsx
+++ src/pages/qcomps/updObjectsForm.tsx
@@ -1,49 +1,41 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Scoreboard() {
   const [player, setPlayer] = useState({
-    firstName: 'John Woodrow',
-    lastName: 'Wilson',
+    firstName: "John Woodrow",
+    lastName: "Wilson",
     likescore: 10,
   });
 
   function handlePlusClick() {
-    player.likescore++;
+    const nextPlayer = { ...player, likescore: player.likescore + 1 };
+    setPlayer(nextPlayer);
   }
 
-  function handleFirstNameChange(e: { target: { value: string; }; }) {
-    setPlayer({
-      ...player,
-      firstName: e.target.value,
-    });
+  function handleFirstNameChange(e: { target: { value: string } }) {
+    const nextPlayer = { ...player, firstName: e.target.value };
+    setPlayer(nextPlayer);
   }
 
-  function handleLastNameChange(e: { target: { value: string; }; }) {
-    player.lastName = e.target.value;
+  function handleLastNameChange(e: { target: { value: string } }) {
+    const nextPlayer = { ...player, lastName: e.target.value };
+    setPlayer(nextPlayer);
   }
 
   return (
     <>
       <label>
         Like Score: <b>{player.likescore}</b>
-        {'  '}
-        <button onClick={handlePlusClick}>
-          +1
-        </button>
+        {"  "}
+        <button onClick={handlePlusClick}>+1</button>
       </label>
       <label>
         First name:
-        <input
-          value={player.firstName}
-          onChange={handleFirstNameChange}
-        />
+        <input value={player.firstName} onChange={handleFirstNameChange} />
       </label>
       <label>
         Last name:
-        <input
-          value={player.lastName}
-          onChange={handleLastNameChange}
-        />
+        <input value={player.lastName} onChange={handleLastNameChange} />
       </label>
     </>
   );
