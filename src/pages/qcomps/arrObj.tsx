5d7dc5f01f5350f6caa189d8d0202049da0924c1
varunmohan05 <varunmohan05@gmail.com>
2024-10-06 23:24:18 -0400

    Fixed arrObj.tsx

--- src/pages/qcomps/arrObj.tsx
+++ src/pages/qcomps/arrObj.tsx
@@ -1,36 +1,53 @@
-import { useState } from 'react';
-import { ItemListProps } from '../../types/arrObj';
+import { useState } from "react";
+import { ItemListProps } from "../../types/arrObj";
 
 let nextId = 3;
-const initialList = [
-  { id: 0, title: 'Big Bellies', seen: false },
-  { id: 1, title: 'Lunar Landscape', seen: false },
-  { id: 2, title: 'Terracotta Army', seen: true },
+type artist = {
+  id: number;
+  title: string;
+  seen: boolean;
+};
+const initialList: artist[] = [
+  { id: 0, title: "Big Bellies", seen: false },
+  { id: 1, title: "Lunar Landscape", seen: false },
+  { id: 2, title: "Terracotta Army", seen: true },
 ];
 
 export default function BucketList() {
   const [myList, setMyList] = useState(initialList);
-  const [yourList, setYourList] = useState(
-    initialList
-  );
+  const [yourList, setYourList] = useState(initialList);
+
+  // created a new function to reduce code duplicacy
+  function createUpdatedCopyOfList(
+    artistList: artist[],
+    artworkId: number,
+    nextSeen: boolean
+  ): artist[] {
+    const tmpList = artistList.map((e) => {
+      const nextArtist = { ...e };
+      if (e.id === artworkId) {
+        nextArtist.seen = nextSeen;
+      }
+      return nextArtist;
+    });
+    return tmpList;
+  }
 
   function handleToggleMyList(artworkId: number, nextSeen: boolean) {
-    const tmpList = myList.map(e => {
-        if (e.id === artworkId) {
-            e.seen = nextSeen
-        }
-        return e
-    });
+    const tmpList: artist[] = createUpdatedCopyOfList(
+      myList,
+      artworkId,
+      nextSeen
+    );
     setMyList(tmpList);
   }
 
   function handleToggleYourList(artworkId: number, nextSeen: boolean) {
-    const tmpList = yourList.map(e => {
-        if (e.id === artworkId) {
-            e.seen = nextSeen
-        }
-        return e
-    });
+    const tmpList: artist[] = createUpdatedCopyOfList(
+      yourList,
+      artworkId,
+      nextSeen
+    );
     setYourList(tmpList);
   }
 
@@ -38,13 +55,9 @@ export default function BucketList() {
     <>
       <h1>Art Bucket List</h1>
       <h2>My list of art to see:</h2>
-      <ItemList
-        artworks={myList}
-        onToggle={handleToggleMyList} />
+      <ItemList artworks={myList} onToggle={handleToggleMyList} />
       <h2>Your list of art to see:</h2>
-      <ItemList
-        artworks={yourList}
-        onToggle={handleToggleYourList} />
+      <ItemList artworks={yourList} onToggle={handleToggleYourList} />
     </>
   );
 }
@@ -52,17 +65,14 @@ export default function BucketList() {
 function ItemList({ artworks, onToggle }: ItemListProps) {
   return (
     <ul>
-      {artworks.map(artwork => (
+      {artworks.map((artwork) => (
         <li key={artwork.id}>
           <label>
             <input
               type="checkbox"
               checked={artwork.seen}
-              onChange={e => {
-                onToggle(
-                  artwork.id,
-                  e.target.checked
-                );
+              onChange={(e) => {
+                onToggle(artwork.id, e.target.checked);
               }}
             />
             {artwork.title}
